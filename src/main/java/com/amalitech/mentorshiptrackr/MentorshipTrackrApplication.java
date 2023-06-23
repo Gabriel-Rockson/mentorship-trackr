595ff1305ca383eaf57223d04baa2c9fca87337a
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-06-23 09:18:51 +0000

    feat: Initial setup of project

---
+++ src/main/java/com/amalitech/mentorshiptrackr/MentorshipTrackrApplication.java
@@ -0,0 +1,13 @@
+package com.amalitech.mentorshiptrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorshipTrackrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorshipTrackrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amalitech/mentorshiptrackr/MentorshipTrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalitech.mentorshiptrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorshipTrackrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
