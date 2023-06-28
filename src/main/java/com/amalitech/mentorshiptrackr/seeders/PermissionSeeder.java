2673d8893ca03482cb0ea5582b2ecb6813aa4a15
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-06-28 17:40:59 +0000

    feat: Add command line runner to seed db with permissions
    Added a command line runer to seed the following permissions in the
    database
    - Manage mentorship
    - View mentorship

---
+++ src/main/java/com/amalitech/mentorshiptrackr/seeders/PermissionSeeder.java
@@ -0,0 +1,49 @@
+package com.amalitech.mentorshiptrackr.seeders;
+
+import com.amalitech.mentorshiptrackr.models.Permission;
+import com.amalitech.mentorshiptrackr.services.PermissionService;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import org.slf4j.Logger;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Order(1)
+public class PermissionSeeder implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(PermissionSeeder.class);
+
+    @Autowired
+    private PermissionService permissionService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("The Permission seeder is running");
+
+        List<Permission> permissions = new ArrayList<>();
+        permissions.add(
+                Permission.builder()
+                        .name("Manage mentorship")
+                        .description("create, view, update and delete on mentorship(advisors and advisees)")
+                        .build()
+        );
+        permissions.add(
+                Permission.builder()
+                        .name("View mentorship")
+                        .description("view mentorship only")
+                        .build()
+        );
+
+        for (Permission permission : permissions) {
+            permission = permissionService.createPermissionIfNotExists(permission);
+            if (permission != null)
+                logger.info("%s permission has been seeded successfully".formatted(permission.getName()));
+        }
+    }
+}
