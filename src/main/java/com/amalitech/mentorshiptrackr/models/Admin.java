f775b187816554aab923e74779d1bda4bb2ca64c
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-06-28 12:28:03 +0000

    refactor: Add column annotations to fields
    Changed the firstname column on Admin entity to first_name

--- src/main/java/com/amalitech/mentorshiptrackr/models/Admin.java
+++ src/main/java/com/amalitech/mentorshiptrackr/models/Admin.java
@@ -2,11 +2,15 @@ package com.amalitech.mentorshiptrackr.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.UUID;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "admins")
 public class Admin {
@@ -17,14 +21,17 @@ public class Admin {
 
     @NotBlank(message = "Username is required")
     @Getter @Setter
+    @Column(name = "username", nullable = false)
     private String username;
 
     @NotBlank(message = "First name is required")
     @Getter @Setter
-    private String firstname;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
 
     @NotBlank(message = "Email is required")
     @Getter @Setter
+    @Column(name = "email", nullable = false)
     private String email;
 
     @Getter @Setter
