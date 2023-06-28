58a34a29308d7facb1260b1c822dcf633fa3c787
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-06-28 12:28:03 +0000

    refactor: Add column annotations to fields
    Changed the firstname column on Admin entity to first_name

--- src/main/java/com/amalitech/mentorshiptrackr/models/Permission.java
+++ src/main/java/com/amalitech/mentorshiptrackr/models/Permission.java
@@ -1,12 +1,16 @@
 package com.amalitech.mentorshiptrackr.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Collection;
 import java.util.UUID;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "permissions")
 public class Permission {
@@ -16,9 +20,11 @@ public class Permission {
     private UUID id;
 
     @Getter @Setter
+    @Column(name = "name", nullable = false)
     private String name;
 
     @Getter @Setter
+    @Column(name = "description", nullable = false)
     private String description;
 
     @Getter @Setter
--- src/main/java/com/amalitech/mentorshiptrackr/models/Role.java
+++ src/main/java/com/amalitech/mentorshiptrackr/models/Role.java
@@ -2,13 +2,17 @@ package com.amalitech.mentorshiptrackr.models;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Collection;
 import java.util.Set;
 import java.util.UUID;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "roles")
 public class Role {
@@ -18,9 +22,11 @@ public class Role {
     private UUID id;
 
     @Getter @Setter
+    @Column(name = "name", nullable = false)
     private String name;
 
     @Getter @Setter
+    @Column(name = "description", nullable = false)
     private String description;
 
     @Getter @Setter
