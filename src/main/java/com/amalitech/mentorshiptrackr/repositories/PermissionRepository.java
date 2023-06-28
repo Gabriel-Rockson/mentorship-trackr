c3a1f918cadb471a6d771b2879e342e67cda4c9c
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-06-28 17:36:27 +0000

    feat: Add permission repository later to communicate with db

---
+++ src/main/java/com/amalitech/mentorshiptrackr/repositories/PermissionRepository.java
@@ -0,0 +1,10 @@
+package com.amalitech.mentorshiptrackr.repositories;
+
+import com.amalitech.mentorshiptrackr.models.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PermissionRepository extends JpaRepository<Permission, UUID> {
+    Permission findByNameIgnoreCase(String permissionName);
+}
