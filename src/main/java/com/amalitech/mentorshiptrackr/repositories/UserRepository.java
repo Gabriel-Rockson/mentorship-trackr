b088973c548782872ae7e3e04350056e642a4f7a
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-06-30 11:33:12 +0000

    feat: Add repository layer for user Entity to communicate with db
    Signed-off-by: Gabriel Rockson <gabriel.rockson@amalitech.com>

---
+++ src/main/java/com/amalitech/mentorshiptrackr/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.amalitech.mentorshiptrackr.repositories;
+
+import com.amalitech.mentorshiptrackr.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findUserByEmail(String userEmail);
+}
