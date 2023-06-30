86c0433f788a11916147ba7a59b86f82fdfb5603
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-06-30 10:41:20 +0000

    feat: Add User entity and users table
    Added users entity to hold the common fields (username, email, and password) that'll be required for user authentication later on in the project.
    
    Added users table migration file

---
+++ src/main/java/com/amalitech/mentorshiptrackr/models/User.java
@@ -0,0 +1,33 @@
+package com.amalitech.mentorshiptrackr.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter @Setter
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank(message = "Username is required")
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    private @Column(name = "password", nullable = false)
+    String password;
+}
