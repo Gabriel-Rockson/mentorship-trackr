786349aac303cdeb3a3046648cefa27e8928a98a
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-06-30 10:55:29 +0000

    feat: Modify Admin entity to inherit User entity

--- src/main/java/com/amalitech/mentorshiptrackr/models/Admin.java
+++ src/main/java/com/amalitech/mentorshiptrackr/models/Admin.java
@@ -4,36 +4,18 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
-import java.util.UUID;
-
-
-@Getter @Setter
-@AllArgsConstructor
+@Getter
+@Setter
 @NoArgsConstructor
-@Builder
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 @Entity
 @Table(name = "admins")
-public class Admin {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
-
-    @NotBlank(message = "Username is required")
-    @Column(name = "username", nullable = false, unique = true)
-    private String username;
-
+public class Admin extends User {
     @NotBlank(message = "First name is required")
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @NotBlank(message = "Email is required")
-    @Column(name = "email", nullable = false, unique = true)
-    private String email;
-
-    @Column(name = "password", nullable = false)
-    private String password;
-
-    @NotBlank(message = "Role is required")
     @ManyToOne
     @JoinColumn(name = "role_id", nullable = false)
     private Role role;
