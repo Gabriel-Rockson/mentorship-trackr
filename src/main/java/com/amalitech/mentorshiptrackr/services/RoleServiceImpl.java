a06a2574c18628884efe78eee1741aecf2174862
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-03 11:09:17 +0000

    refactor: Check for the existence of a Role before creating the role
    Added a check in the Role service to check for the existence of Roles
    before creating new ones.
    Also a RoleAlreadyExistsException is thrown if the role is already
    in the database.
    
    Signed-off-by: Gabriel Rockson <gabriel.rockson@amalitech.com>

--- src/main/java/com/amalitech/mentorshiptrackr/services/RoleServiceImpl.java
+++ src/main/java/com/amalitech/mentorshiptrackr/services/RoleServiceImpl.java
@@ -1,7 +1,9 @@
 package com.amalitech.mentorshiptrackr.services;
 
+import com.amalitech.mentorshiptrackr.exceptions.RoleAlreadyExistsException;
 import com.amalitech.mentorshiptrackr.models.Role;
 import com.amalitech.mentorshiptrackr.repositories.RoleRepository;
+import com.amalitech.mentorshiptrackr.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,19 +13,24 @@ import org.springframework.stereotype.Service;
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
 
     @Override
     @Transactional
-    public Role createRoleIfNotExists(Role role) {
-        Role roleInDB = roleRepository.findByNameIgnoreCase(role.getName());
-
-        if (roleInDB == null) {
-            roleInDB = role;
-            roleRepository.save(roleInDB);
+    public Role addNewRole(Role role) throws RoleAlreadyExistsException {
+        if (roleExists(role.getName())) {
+            throw new RoleAlreadyExistsException("A role with name: %s already exists.".formatted(role.getName()));
         }
 
-        return roleInDB;
+        role = roleRepository.save(role);
+        return role;
     }
+
+    @Override
+    public boolean roleExists(String roleName) {
+        return roleRepository.existsByNameIgnoreCase(roleName);
+    }
+
 
     public Role findRoleByNameIgnoreCase(String roleName) {
         return roleRepository.findByNameIgnoreCase(roleName);
