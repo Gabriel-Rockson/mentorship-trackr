f2a8348dc38251c27db78f04444e51a8546cbc4d
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-05 11:23:34 +0000

    feat: Move the roles column from Admin entity to the upper User entity
    Moved the roles column to the User entity so that subsequent entities
    that extend the User entity can have access to the roles column without
    redefinition.

--- src/main/java/com/amalitech/mentorshiptrackr/models/Admin.java
+++ src/main/java/com/amalitech/mentorshiptrackr/models/Admin.java
@@ -1,6 +1,8 @@
 package com.amalitech.mentorshiptrackr.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
@@ -16,13 +18,8 @@ public class Admin extends User {
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @ManyToOne
-    @JoinColumn(name = "role_id", nullable = false)
-    private Role role;
-
     public Admin(String username, String firstName, String email, String password, Role role) {
-        super(username, email, password);
+        super(username, email, password, role);
         this.firstName = firstName;
-        this.role = role;
     }
 }
