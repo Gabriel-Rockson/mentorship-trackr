6426580084fbdaa92ea57b7d9bf6c431a49aaeb8
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-06 19:07:30 +0000

    feat: Add security filter chain
    Added SecurityFilter chain which authenticates http requests.
    
    Signed-off-by: Gabriel Rockson <gabriel.rockson@amalitech.com>

--- src/main/java/com/amalitech/mentorshiptrackr/config/SecurityConfig.java
+++ src/main/java/com/amalitech/mentorshiptrackr/config/SecurityConfig.java
@@ -1,14 +1,34 @@
 package com.amalitech.mentorshiptrackr.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 public class SecurityConfig {
+
+    @Value("${api.endpoint.base-url}")
+    private String baseUrl;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
 
     @Bean
     public PasswordEncoder encoder() {
