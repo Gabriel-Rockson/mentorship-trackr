e7bb95c96c6feacb76387d6605de7b128ced86b6
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-07 04:18:33 +0000

    feat: Add a new constructor.
    Signed-off-by: Gabriel Rockson <gabriel.rockson@amalitech.com>

--- src/main/java/com/amalitech/mentorshiptrackr/models/User.java
+++ src/main/java/com/amalitech/mentorshiptrackr/models/User.java
@@ -45,4 +45,10 @@ public class User {
         this.password = password;
         this.role = role;
     }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 }
