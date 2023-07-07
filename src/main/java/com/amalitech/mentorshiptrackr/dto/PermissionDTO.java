72a2af1032a49e5ade1822561cff46007f12a84c
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-07 05:32:01 +0000

    feat: Add CreatePermissionDTO and PermissionDTO to structure data
    Added CreatePermissionDTO to structure the incoming permission data
    from the user
    Added PermissionDTO to structure the outgoing permission data to the
    user
    
    Signed-off-by: Gabriel Rockson <gabriel.rockson@amalitech.com>

---
+++ src/main/java/com/amalitech/mentorshiptrackr/dto/PermissionDTO.java
@@ -0,0 +1,18 @@
+package com.amalitech.mentorshiptrackr.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PermissionDTO {
+    private UUID id;
+    private String name;
+    private String description;
+}
