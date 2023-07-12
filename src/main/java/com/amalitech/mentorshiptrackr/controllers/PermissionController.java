1caa8afc92a1cfda6cf1559ec493060cd7c9c0a4
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-12 23:50:44 +0000

    refactor: Modify PermissionController to call appropriate service method
    Modified the permission controller to call the addNewPermission method
    
    Signed-off-by: Gabriel Rockson <gabriel.rockson@amalitech.com>

--- src/main/java/com/amalitech/mentorshiptrackr/controllers/PermissionController.java
+++ src/main/java/com/amalitech/mentorshiptrackr/controllers/PermissionController.java
@@ -1,29 +1,27 @@
 package com.amalitech.mentorshiptrackr.controllers;
 
-import com.amalitech.mentorshiptrackr.dto.CreatePermissionDTO;
-import com.amalitech.mentorshiptrackr.dto.PermissionDTO;
-import com.amalitech.mentorshiptrackr.dto.PermissionMapper;
-import com.amalitech.mentorshiptrackr.models.Permission;
+import com.amalitech.mentorshiptrackr.dto.ResponseHandler;
+import com.amalitech.mentorshiptrackr.dto.request.PermissionRequest;
 import com.amalitech.mentorshiptrackr.services.PermissionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/permissions")
 @RestController
 @RequiredArgsConstructor
 public class PermissionController {
     private final PermissionService permissionService;
-    private final PermissionMapper permissionMapper;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public PermissionDTO addNewRole(@RequestBody @Valid CreatePermissionDTO permissionDTO) {
-        Permission permission = permissionService
-                .addNewPermission(permissionMapper.toEntity(permissionDTO));
-
-        return permissionMapper.toDTO(permission);
+    public ResponseEntity<Object> addNewRole(@RequestBody @Valid PermissionRequest permissionRequest) {
+        return ResponseHandler.successResponse(HttpStatus.CREATED,
+                permissionService.addNewPermission(permissionRequest));
     }
 
 }
