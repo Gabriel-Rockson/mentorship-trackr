e371ce45f91a905419e317c2813a310cc455a450
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-12 14:09:27 +0000

    feat: Add error messages to fields
    Signed-off-by: Gabriel Rockson <gabriel.rockson@amalitech.com>

--- src/main/java/com/amalitech/mentorshiptrackr/models/User.java
+++ src/main/java/com/amalitech/mentorshiptrackr/models/User.java
@@ -1,6 +1,7 @@
 package com.amalitech.mentorshiptrackr.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
