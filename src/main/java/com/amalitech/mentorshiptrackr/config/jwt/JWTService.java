74c3629c74ae987d926acf0ebcb423f3dab78343
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-12 11:34:23 +0000

    feat: Add Filter for JWT Authentication and JWTService
    Added JwTAuthenticationFilter to intercept requests and validate user
    token and authenticate user based on token.
    
    Added JWTService to provide utility functions for generating token and
    extract data from tokens.
    
    Signed-off-by: Gabriel Rockson <gabriel.rockson@amalitech.com>

---
+++ src/main/java/com/amalitech/mentorshiptrackr/config/jwt/JWTService.java
@@ -0,0 +1,83 @@
+package com.amalitech.mentorshiptrackr.config.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+
+@Component
+@RequiredArgsConstructor
+public class JWTService {
+    private final Environment environment;
+
+    public String generateJwtToken(UserDetails userDetails) {
+        return generateJwtToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateJwtToken(Map<String, Object> claims, UserDetails userDetails) {
+        int EXPIRATION_TIME = Integer.parseInt(
+                Objects.requireNonNull(environment.getProperty("JWT_EXPIRATION_TIME_IN_MILLI_SECONDS"))
+        );
+
+        return Jwts
+                .builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+
+        return claimsResolver.apply(claims);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        String SECRET_KEY = Objects.requireNonNull(Objects.requireNonNull(environment.getProperty("JWT_SECRET_KEY")));
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+}
