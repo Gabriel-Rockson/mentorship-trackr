612ca8dd8900d58b586ac24414851b7d215fccd0
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-12 13:44:47 +0000

    feat: Add JwT authentication filter to authenticate users.
    Registered JWTAuthenticationFilter in the filter chain to deal with
    jwt authentication.
    
    Signed-off-by: Gabriel Rockson <gabriel.rockson@amalitech.com>

--- src/main/java/com/amalitech/mentorshiptrackr/config/SecurityConfig.java
+++ src/main/java/com/amalitech/mentorshiptrackr/config/SecurityConfig.java
@@ -1,38 +1,61 @@
 package com.amalitech.mentorshiptrackr.config;
 
+import com.amalitech.mentorshiptrackr.config.jwt.JWTAuthenticationFilter;
+import com.amalitech.mentorshiptrackr.services.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+    private final UserServiceImpl userService;
+    private final PasswordEncoder passwordEncoder;
+
+    private final String ADMINISTRATOR_AUTHORITY = "Administrator";
 
     @Value("${api.endpoint.base-url}")
     private String baseUrl;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authenticationProvider(authenticationProvider());
+        httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         httpSecurity
-                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(baseUrl + "/roles").hasAuthority(ADMINISTRATOR_AUTHORITY).requestMatchers(baseUrl + "/admins").hasAuthority(ADMINISTRATOR_AUTHORITY)
+                        .requestMatchers(baseUrl + "/permissions").hasAuthority(ADMINISTRATOR_AUTHORITY)
+                        .requestMatchers(baseUrl + "/authentication/token").permitAll()
                         .anyRequest().authenticated()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults());
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return httpSecurity.build();
     }
 
     @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+        daoAuthenticationProvider.setUserDetailsService(userService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return daoAuthenticationProvider;
     }
+
 }
