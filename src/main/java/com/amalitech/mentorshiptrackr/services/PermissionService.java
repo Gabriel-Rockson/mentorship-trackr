9ba89bba2b88b1c9121e726feba923f0c74ccd0f
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-12 23:49:14 +0000

    refactor: Modify the add new permission service and seeder to use dtos
    Modified the service and the seeder to use the PermissionRequest and PermissionResponse dtos for the data incoming and outgoing
    
    Signed-off-by: Gabriel Rockson <gabriel.rockson@amalitech.com>

--- src/main/java/com/amalitech/mentorshiptrackr/services/PermissionService.java
+++ src/main/java/com/amalitech/mentorshiptrackr/services/PermissionService.java
@@ -1,5 +1,7 @@
 package com.amalitech.mentorshiptrackr.services;
 
+import com.amalitech.mentorshiptrackr.dto.request.PermissionRequest;
+import com.amalitech.mentorshiptrackr.dto.response.PermissionResponse;
 import com.amalitech.mentorshiptrackr.exceptions.EntityAlreadyExistsException;
 import com.amalitech.mentorshiptrackr.models.Permission;
 import jakarta.persistence.EntityNotFoundException;
@@ -12,9 +14,9 @@ public interface PermissionService {
     Optional<Permission> findByNameIgnoreCase(String roleName) throws EntityNotFoundException;
 
     @Transactional
-    Permission addNewPermission(Permission permission) throws EntityAlreadyExistsException;
+    PermissionResponse addNewPermission(PermissionRequest permissionRequest) throws EntityAlreadyExistsException;
 
     boolean permissionExists(String permissionName);
 
-    void seedPermissions(Set<Permission> permissions);
+    void seedPermissions(Set<PermissionRequest> permissions);
 }
