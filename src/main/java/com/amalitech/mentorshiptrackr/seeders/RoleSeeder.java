f390003f08778789ce28b2d7dc465f8550f19985
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-12 23:03:51 +0000

    feat: Move Role seeding logic from RoleSeeder to RoleService
    Added new method that just takes the roles to be seeded and handles that logic.
    
    Refactored the addNewRole method to work for both seeding role and also
    adding new role through the controller
    
    Signed-off-by: Gabriel Rockson <gabriel.rockson@amalitech.com>

--- src/main/java/com/amalitech/mentorshiptrackr/seeders/RoleSeeder.java
+++ src/main/java/com/amalitech/mentorshiptrackr/seeders/RoleSeeder.java
@@ -1,65 +1,43 @@
 package com.amalitech.mentorshiptrackr.seeders;
 
-import com.amalitech.mentorshiptrackr.exceptions.EntityAlreadyExistsException;
-import com.amalitech.mentorshiptrackr.models.Permission;
-import com.amalitech.mentorshiptrackr.models.Role;
-import com.amalitech.mentorshiptrackr.services.PermissionService;
+import com.amalitech.mentorshiptrackr.dto.request.RoleRequest;
 import com.amalitech.mentorshiptrackr.services.RoleService;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 @Component
 @Order(2)
 @RequiredArgsConstructor
 public class RoleSeeder implements CommandLineRunner {
-    private static final Logger logger = LoggerFactory.getLogger(RoleSeeder.class);
-    private final PermissionService permissionService;
     private final RoleService roleService;
 
     @Override
     public void run(String... args) throws Exception {
-        Set<Role> roles = new HashSet<>();
+        Set<RoleRequest> roles = new HashSet<>();
 
-        roles.add(Role.builder()
+        roles.add(RoleRequest.builder()
                 .name("Administrator")
                 .description("Perform all actions")
                 .build()
         );
 
-        Set<Permission> mentorshipManagerPermissions = new HashSet<>();
+        Set<String> mentorshipManagerPermissions = new HashSet<>();
 
+        mentorshipManagerPermissions.add("manage mentorship");
+        mentorshipManagerPermissions.add("view mentorship");
 
-        Optional<Permission> manageMentorshipPermission = permissionService.findByNameIgnoreCase("manage mentorship");
-        Optional<Permission> viewMentorshipPermission = permissionService.findByNameIgnoreCase("view mentorship");
-
-        manageMentorshipPermission.ifPresent(mentorshipManagerPermissions::add);
-        viewMentorshipPermission.ifPresent(mentorshipManagerPermissions::add);
-
-        roles.add(Role.builder()
+        roles.add(RoleRequest.builder()
                 .name("Mentorship manager")
                 .description("Perform mentorship associated CRUD actions")
                 .permissions(mentorshipManagerPermissions)
                 .build()
         );
 
-        for (Role role : roles) {
-            try {
-                role = roleService.addNewRole(role);
-                logger.info("'{}' role has been seeded successfully.", role.getName());
-            } catch (EntityAlreadyExistsException exception) {
-                logger.info("'{}' role already seeded.", role.getName());
-            } catch (Exception exception) {
-                logger.error("Something went wrong during seeding role: {}", exception.getMessage());
-            }
-
-        }
+        roleService.seedRoles(roles);
     }
 }
--- src/main/java/com/amalitech/mentorshiptrackr/services/RoleService.java
+++ src/main/java/com/amalitech/mentorshiptrackr/services/RoleService.java
@@ -1,15 +1,22 @@
 package com.amalitech.mentorshiptrackr.services;
 
+import com.amalitech.mentorshiptrackr.dto.request.RoleRequest;
+import com.amalitech.mentorshiptrackr.dto.response.RoleResponse;
 import com.amalitech.mentorshiptrackr.exceptions.EntityAlreadyExistsException;
 import com.amalitech.mentorshiptrackr.models.Role;
 import jakarta.transaction.Transactional;
+
+import java.util.Set;
 
 public interface RoleService {
 
     Role findRoleByNameIgnoreCase(String roleName);
 
     @Transactional
-    Role addNewRole(Role role) throws EntityAlreadyExistsException;
+    RoleResponse addNewRole(RoleRequest roleRequest) throws EntityAlreadyExistsException;
 
     boolean roleExists(String roleName);
+
+    @Transactional
+    void seedRoles(Set<RoleRequest> roleRequests);
 }
