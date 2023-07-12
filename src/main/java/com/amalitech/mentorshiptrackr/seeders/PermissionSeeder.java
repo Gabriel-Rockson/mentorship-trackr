151cc578a3b5508b4f300c4f4162156d20e053ea
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-12 23:13:39 +0000

    refactor: Move permission seeding logic to permissions service
    Signed-off-by: Gabriel Rockson <gabriel.rockson@amalitech.com>

--- src/main/java/com/amalitech/mentorshiptrackr/seeders/PermissionSeeder.java
+++ src/main/java/com/amalitech/mentorshiptrackr/seeders/PermissionSeeder.java
@@ -1,11 +1,8 @@
 package com.amalitech.mentorshiptrackr.seeders;
 
-import com.amalitech.mentorshiptrackr.exceptions.EntityAlreadyExistsException;
 import com.amalitech.mentorshiptrackr.models.Permission;
 import com.amalitech.mentorshiptrackr.services.PermissionService;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -17,28 +14,22 @@ import java.util.Set;
 @Order(1)
 @RequiredArgsConstructor
 public class PermissionSeeder implements CommandLineRunner {
-
-    private static final Logger logger = LoggerFactory.getLogger(PermissionSeeder.class);
-
     private final PermissionService permissionService;
 
     @Override
     public void run(String... args) throws Exception {
 
         Set<Permission> permissions = new HashSet<>();
-        permissions.add(Permission.builder().name("Manage mentorship").description("create, view, update and delete on mentorship(advisors and advisees)").build());
-        permissions.add(Permission.builder().name("View mentorship").description("view mentorship only").build());
+        permissions.add(Permission.builder()
+                .name("Manage mentorship")
+                .description("create, view, update and delete on mentorship(advisors and advisees)").build()
+        );
+        permissions.add(Permission.builder()
+                .name("View mentorship")
+                .description("view mentorship only")
+                .build()
+        );
 
-
-        for (Permission permission : permissions) {
-            try {
-                permission = permissionService.addNewPermission(permission);
-                logger.info("'{}' permission has been seeded successfully.", permission.getName());
-            } catch (EntityAlreadyExistsException exception) {
-                logger.info("'{}' permission already seeded.", permission.getName());
-            } catch (Exception exception) {
-                logger.error(exception.getMessage());
-            }
-        }
+        permissionService.seedPermissions(permissions);
     }
 }
--- src/main/java/com/amalitech/mentorshiptrackr/services/PermissionServiceImpl.java
+++ src/main/java/com/amalitech/mentorshiptrackr/services/PermissionServiceImpl.java
@@ -5,15 +5,21 @@ import com.amalitech.mentorshiptrackr.models.Permission;
 import com.amalitech.mentorshiptrackr.repositories.PermissionRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class PermissionServiceImpl implements PermissionService {
 
+    private static final Logger logger = LoggerFactory.getLogger(PermissionServiceImpl.class);
     private final PermissionRepository permissionRepository;
 
     @Override
@@ -30,6 +36,20 @@ public class PermissionServiceImpl implements PermissionService {
 
     public boolean permissionExists(String permissionName) {
         return permissionRepository.existsByNameIgnoreCase(permissionName);
+    }
+
+    @Override
+    public void seedPermissions(@NotNull @Valid Set<Permission> permissions) {
+        for (Permission permission : permissions) {
+            try {
+                permission = addNewPermission(permission);
+                logger.info("'{}' permission has been seeded successfully.", permission.getName());
+            } catch (EntityAlreadyExistsException exception) {
+                logger.info("'{}' permission already seeded.", permission.getName());
+            } catch (Exception exception) {
+                logger.error(exception.getMessage());
+            }
+        }
     }
 
     @Override
