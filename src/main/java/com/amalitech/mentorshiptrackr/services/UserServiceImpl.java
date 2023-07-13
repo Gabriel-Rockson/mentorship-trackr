ef66d113a2e483f3396a1c1df9d58613b4a0a234
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-13 07:58:17 +0000

    feat: Add user service method to add a new advisee account
    Signed-off-by: Gabriel Rockson <gabriel.rockson@amalitech.com>

--- src/main/java/com/amalitech/mentorshiptrackr/services/UserServiceImpl.java
+++ src/main/java/com/amalitech/mentorshiptrackr/services/UserServiceImpl.java
@@ -2,19 +2,20 @@ package com.amalitech.mentorshiptrackr.services;
 
 import com.amalitech.mentorshiptrackr.dto.UserPrincipal;
 import com.amalitech.mentorshiptrackr.dto.mapper.AdminMapper;
+import com.amalitech.mentorshiptrackr.dto.mapper.AdviseeMapper;
 import com.amalitech.mentorshiptrackr.dto.mapper.AdvisorMapper;
 import com.amalitech.mentorshiptrackr.dto.request.AdminRequest;
+import com.amalitech.mentorshiptrackr.dto.request.AdviseeRequest;
 import com.amalitech.mentorshiptrackr.dto.request.AdvisorRequest;
 import com.amalitech.mentorshiptrackr.dto.response.AdminResponse;
+import com.amalitech.mentorshiptrackr.dto.response.AdviseeResponse;
 import com.amalitech.mentorshiptrackr.dto.response.AdvisorResponse;
 import com.amalitech.mentorshiptrackr.exceptions.EntityAlreadyExistsException;
-import com.amalitech.mentorshiptrackr.models.Admin;
-import com.amalitech.mentorshiptrackr.models.Advisor;
-import com.amalitech.mentorshiptrackr.models.Role;
-import com.amalitech.mentorshiptrackr.models.User;
+import com.amalitech.mentorshiptrackr.models.*;
 import com.amalitech.mentorshiptrackr.repositories.RoleRepository;
 import com.amalitech.mentorshiptrackr.repositories.UserRepository;
 import com.amalitech.mentorshiptrackr.utils.PasswordGenerator;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +25,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 
 @Service
@@ -36,6 +39,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     private final RoleRepository roleRepository;
     private final AdvisorMapper advisorMapper;
     private final AdminMapper adminMapper;
+    private final AdviseeMapper adviseeMapper;
 
     private void checkAccountExistence(String username, String email) {
         if (usernameExists(username)) {
@@ -112,6 +116,31 @@ public class UserServiceImpl implements UserService, UserDetailsService {
         logger.info("'The password generated for the user with email {} is {}", adminRequest.getEmail(), password);
 
         return adminMapper.toAdminResponse(admin);
+    }
+
+    public AdviseeResponse addNewAdviseeAccount(AdviseeRequest adviseeRequest) throws EntityAlreadyExistsException {
+        checkAccountExistence(adviseeRequest.getUsername(), adviseeRequest.getEmail());
+
+        String password = PasswordGenerator.generatePassword(8);
+
+        Advisee advisee = adviseeMapper.toAdvisee(adviseeRequest);
+        advisee.setPassword(passwordEncoder.encode(password));
+
+        if (adviseeRequest.getAdvisorId() != null) {
+            Optional<User> advisor = Optional.ofNullable(userRepository.findById(adviseeRequest.getAdvisorId()).orElseThrow(
+                    () -> new EntityNotFoundException("Advisor with id " + adviseeRequest.getAdvisorId() + " not " +
+                            "found.")
+            ));
+            advisor.ifPresent(user -> advisee.setAdvisor((Advisor) user));
+        }
+
+        userRepository.save(advisee);
+
+        if (advisee.getId() != null) {
+            logger.info("Password for Advisee account with email: {} is {}", advisee.getEmail(), password);
+        }
+
+        return adviseeMapper.toAdviseeResponse(advisee);
     }
 
     // TODO: allow email to be used for login also as `username`
