90e08f9151597c4e69e76fbb5a047ac48d63553c
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-13 07:58:49 +0000

    feat: Add controller to add new Advisees account
    Signed-off-by: Gabriel Rockson <gabriel.rockson@amalitech.com>

---
+++ src/main/java/com/amalitech/mentorshiptrackr/controllers/AdviseeController.java
@@ -0,0 +1,26 @@
+package com.amalitech.mentorshiptrackr.controllers;
+
+import com.amalitech.mentorshiptrackr.dto.ResponseHandler;
+import com.amalitech.mentorshiptrackr.dto.request.AdviseeRequest;
+import com.amalitech.mentorshiptrackr.services.UserServiceImpl;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/advisees")
+@RequiredArgsConstructor
+public class AdviseeController {
+    private final UserServiceImpl userService;
+
+    @PostMapping("/add-new-account")
+    public ResponseEntity<Object> addNewAdviseeAccount(@NotNull @Valid @RequestBody AdviseeRequest adviseeRequest) {
+        return ResponseHandler.successResponse(HttpStatus.CREATED, userService.addNewAdviseeAccount(adviseeRequest));
+    }
+}
